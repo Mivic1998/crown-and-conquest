475eb02e3b996059a116536bfa6b05bdf1bd6820
Mivic1998 <maxivic1998@gmail.com>
2026-06-21 14:54:34 +0100

    Configure PostgreSQL and Environment Variables

--- crown_and_conquest/settings.py
+++ crown_and_conquest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pll03&g8c+8i@&62f0_me6pzb11d%)98)b%51b6qfw4wy63008'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
